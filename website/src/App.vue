4a0189022a8413167a937af7233974629709f6c4
jaywcjlove <398188662@qq.com>
2023-10-20 17:23:35 +0800

    website: build a document website using vite.

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,30 +1,17 @@
-<template>
-  <div>
-    <header class="banner">
-      <img alt="Vue logo" src="./assets/logo.png" />
-      <a href="https://github.com/uivjs/vue-layout" target="__blank">Vue Layout</a>
-      <p>{{ description }}</p>
-    </header>
-    <github-corners target="__blank" fixed href="https://github.com/uivjs/vue-layout" />
-    <example-base />
-    <markdown-preview class="markdown">
-      {{ markdown }}
-    </markdown-preview>
-  </div>
-</template>
 <script>
 import { defineComponent } from 'vue';
+import pkg from '@uivjs/vue-layout/package.json';
 import GithubCorners from '@uivjs/vue-github-corners';
 import MarkdownPreview from '@uivjs/vue-markdown-preview';
-import ExampleBase from './Example.vue';
 import '@uivjs/vue-markdown-preview/markdown.css';
-import str from '@uivjs/vue-layout/README.md';
-import pkg from '@uivjs/vue-layout/package.json';
+import ExampleBase from './Example.vue';
+import mdStr from '../../README.md';
 
 export default defineComponent({
   data() {
     return {
-      markdown: str,
+      repopage: pkg.repository.url,
+      markdown: mdStr,
       description: pkg.description,
     };
   },
@@ -35,8 +22,26 @@ export default defineComponent({
   },
 });
 </script>
-<style>
-.banner {
+
+<template>
+  <div>
+    <github-corners fixed target="__blank" :href="repopage" />
+    <header>
+      <div>
+        <img src="./assets/vue.svg" alt="Vue Monorepo Template" />
+      </div>
+      <a :href="repopage" target="__blank">Vue Monorepo Template</a>
+      <p>{{description}}</p>
+    </header>
+    <example-base></example-base>
+    <markdown-preview class="markdown">
+      {{ markdown }}
+    </markdown-preview>
+  </div>
+</template>
+
+<style scoped>
+header {
   text-align: center;
   min-height: calc(100vh - 130px);
   display: flex;
@@ -46,29 +51,31 @@ export default defineComponent({
   font-size: calc(10px + 2vmin);
   color: #fff;
 }
-.banner img {
+header img {
   height: 20vmin;
 }
-.banner a {
+header a {
   color: #09d3ac;
   font-weight: bold;
 }
-.banner p {
+header p {
   margin-bottom: 10px;
   padding: 10px;
   max-width: 560px;
   white-space: pre-wrap;
   word-wrap: break-word;
 }
-body,
-html {
-  margin: 0;
-  padding: 0;
-  background-color: #282c34;
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
 }
+
 .markdown {
   margin: 0 auto;
-  max-width: 620px;
+  max-width: 680px;
   padding: 23px;
   border-radius: 5px;
   margin-bottom: 110px;
---
+++ website/vite.config.ts
@@ -0,0 +1,97 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import raw from 'vite-raw-plugin';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/vue-layout',
+  plugins: [
+    vue(),
+    raw({
+      fileRegex: /\.md$/,
+    }),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: 'dist/deps.html', //分析图生成的文件名
+      open: true, //如果存在本地服务端口，将在打包后自动展示
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue'],
+          'vue-layout': ['@uivjs/vue-layout'],
+          'hast-util': [
+            'hast-util-has-property',
+            'hast-util-heading-rank',
+            'hast-util-is-element',
+            'hast-util-to-string',
+            'hast-util-parse-selector',
+            'hast-util-from-parse5',
+            'hast-util-raw',
+            'hast-util-to-parse5',
+            'hast-util-select',
+            'hast-util-whitespace',
+            'hast-util-to-html',
+            'hastscript',
+            'mdast-util-to-hast',
+          ],
+          unified: [
+            'unified',
+            'comma-separated-tokens',
+            'property-information',
+            'remark-gfm',
+            'remark-parse',
+            'remark-rehype',
+            'space-separated-tokens',
+            'style-to-object',
+            'refractor',
+            'rehype-attr',
+            'rehype-autolink-headings',
+            'rehype-parse',
+            'rehype-raw',
+            'rehype-rewrite',
+            'rehype-slug',
+            'rehype-stringify',
+            'remark-rehype',
+          ],
+          'rehype-prism-plus': ['rehype-prism-plus'],
+          'micromark-extension': [
+            'micromark',
+            'micromark-util-character',
+            'micromark-extension-gfm',
+            'micromark-extension-gfm-autolink-literal',
+            'micromark-extension-gfm-footnote',
+            'micromark-core-commonmark',
+            'micromark-factory-destination',
+            'micromark-factory-label',
+            'micromark-factory-title',
+            'micromark-factory-whitespace',
+            'micromark-factory-space',
+            'micromark-extension-gfm-strikethrough',
+            'micromark-extension-gfm-table',
+            'micromark-extension-gfm-tagfilter',
+            'micromark-extension-gfm-task-list-item',
+            'micromark-util-combine-extensions',
+            'micromark-util-types',
+            'micromark-util-decode-numeric-character-reference',
+            'micromark-util-decode-string',
+            'micromark-util-normalize-identifier',
+            'micromark-util-chunked',
+            'micromark-util-classify-character',
+            'micromark-util-resolve-all',
+            'micromark-util-html-tag-name',
+            'micromark-util-encode',
+            'micromark-util-subtokenize',
+            'micromark-util-sanitize-uri',
+          ],
+        },
+      },
+    },
+  },
+});
